2edb534cf8ae761e63f7d0982cd8fd79712966b9
jackjianguo <zhaojianguo@outlook.com>
2021-01-04 19:19:15 +0800

    try install easy

---
+++ install-prod.sh
@@ -0,0 +1,13 @@
+conda env remove --name catalyst
+
+conda create --name catalyst python=3.6 scipy zlib
+
+conda activate catalyst
+
+pip install setuptools==45
+
+conda install --name catalyst --freeze-installed bcolz
+
+pip install enigma-catalyst matplotlib
+
+pip install numpy==1.17.0
