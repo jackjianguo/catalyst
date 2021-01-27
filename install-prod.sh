7bbfa03e94091a29e16f9403877f0ecfa6fc033d
zjg <zhaojianguo@outlook.com>
2021-01-27 10:54:14 +0800

    update

--- install-prod.sh
+++ install-prod.sh
@@ -24,6 +24,17 @@ pip freeze
 
 pip install -r requestments.txt
 
+# unzip market data
+tar -zxvf market/catalyst.tar.gz
+mv .catalyst ~/
 
+touch -c ~/.catalyst/data/exchanges/binance/cctx_markets.json
+
+# ingest data
+catalyst ingest-exchange -x binance -f minute --csv market/binance_future_btcusdt_minute.csv --show-progress
+
+# run
+catalyst run -f demo/buy_btc_rt.py --data-frequency minute -x binance -s 2020-01-01 -e 2020-01-01\
+-c usdt --capital-base 1000 -o test.csv
 
 
